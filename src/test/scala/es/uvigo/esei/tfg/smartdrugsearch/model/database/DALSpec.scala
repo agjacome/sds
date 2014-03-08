7901188d72a7bebd2e33ae32a12668a7d94a4c56
Alberto G. Jácome <agjacome@gmail.com>
2014-03-08 22:21:13 +0100

    Implement pending tests in DAL Spec
    Pending tests for testing CRUD in Named Entities and Annotations tables have
    been implemented. Added two more pending tests for retrieving Documents and
    Named Entities directly from the Annotations table: should create by itself the
    Document or Named Entity instance based using its foreign key.

--- src/test/scala/es/uvigo/esei/tfg/smartdrugsearch/model/database/DALSpec.scala
+++ src/test/scala/es/uvigo/esei/tfg/smartdrugsearch/model/database/DALSpec.scala
@@ -61,9 +61,116 @@ class DALSpec extends BaseSpec {
         }
       }
 
-      "CRUDing named entities" in (pending)
+      "CRUDing named entities" in new WithApplication {
+        val dal = DAL(DB("test").driver)
 
-      "CRUDing annotations" in (pending)
+        import Category._
+        import dal._
+        import dal.profile.simple._
+
+        DB("test") withSession { implicit session : Session =>
+          info("creating entities table")
+          NamedEntities.ddl.create
+
+          info("inserting entities")
+          NamedEntities ++= Seq(
+            NamedEntity(None, "Normalized One",   Drug,      0),
+            NamedEntity(None, "Normalized Two",   Species,  13),
+            NamedEntity(None, "Normalized Three", Compound, 21)
+          )
+
+          info("retrieving all entities")
+          NamedEntities.list should have size (3)
+          all (NamedEntities.list map (_.id)) should be ('defined)
+
+          info("searching for a concrete entity")
+          val second = (NamedEntities filter (_.category is Species)).first
+          second should have (
+            'id          (Some(NamedEntityId(2))),
+            'normalized  (Sentence("Normalized Two")),
+            'category    (Species),
+            'occurrences (13)
+          )
+
+          info("updating a named entity information")
+          (for {
+            ent <- NamedEntities
+            if ent.normalized is Sentence("Normalized Three")
+          } yield (ent.occurrences)) update 72
+
+          val third = (NamedEntities filter (_.id is NamedEntityId(3))).first
+          third.occurrences should be (72)
+
+          info("deleting a named entity")
+          (NamedEntities filter (_.id is NamedEntityId(1))).delete
+          (NamedEntities filter (_.category is Drug)).firstOption should be (None)
+
+          info("removing entities table")
+          NamedEntities.ddl.drop
+        }
+      }
+
+      "CRUDing annotations" in new WithApplication {
+        val dal = DAL(DB("test").driver)
+
+        import Category._
+        import dal._
+        import dal.profile.simple._
+
+        DB("test") withSession { implicit session : Session =>
+          info("creating annotations table (with dependencies in foreign keys)")
+          dal.create
+
+          info("inserting annotations (and dependent documents and entities)")
+          Documents ++= Seq(
+            Document(None, "Title One", "this is a non-normalized text one"),
+            Document(None, "Title Two", "also non-normalized text 2")
+          )
+          NamedEntities ++= Seq(
+            NamedEntity(None, "normalized-one", Drug, 1),
+            NamedEntity(None, "normalized-two", Drug, 1)
+          )
+          Annotations ++= Seq(
+            Annotation(None, 1, 1, "non-normalized text one", 10, 33),
+            Annotation(None, 2, 2, "non-normalized text 2", 5, 26)
+          )
+
+          info("retrieving all annotations")
+          Annotations.list should have size(2)
+          all (Annotations.list map (_.id)) should be ('defined)
+
+          info("searching for a concrete annotation")
+          val second = (Annotations filter (_.text is Sentence("non-normalized text 2"))).first
+          second should have (
+            'id       (Some(AnnotationId(2))),
+            'docId    (DocumentId(2)),
+            'entId    (NamedEntityId(2)),
+            'text     (Sentence("non-normalized text 2")),
+            'startPos (5),
+            'endPos   (26)
+          )
+
+          info("updating an annotation's information")
+          (for {
+            annotation <- Annotations
+            if annotation.start is Position(10)
+          } yield (annotation.end)) update 13
+
+          val first = (Annotations filter (_.id is AnnotationId(1))).first
+          first.endPos should be (Position(13))
+
+          info("deleting an annotation")
+          (Annotations filter (_.id is AnnotationId(1))).delete
+          (Annotations filter (_.docId is DocumentId(1))).firstOption should be (None)
+
+          info ("removing annotations table (and all its dependencies)")
+          dal.drop
+        }
+      }
+
+      "obtaining Documents directly from the ForeignKey of Annotations" in (pending)
+
+      "obtaining NamedEntities directly from the ForeignKey of Annotations" in (pending)
 
     }
 
