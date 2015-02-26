0c4d53602fb4a1d19fec918e6153d153f3de647a
Alberto G. Jácome <agjacome@gmail.com>
2015-02-26 13:58:00 +0100

    Remove WebJarAssetsController, use exclusively AssetsController and its 'versioned' method

--- src/main/scala/controller/AssetsController.scala
+++ src/main/scala/controller/AssetsController.scala
@@ -1,15 +1,4 @@
 package es.uvigo.ei.sing.sds.controller
 
-import play.api.http.LazyHttpErrorHandler
-import controllers._
-
-// These objects exist for the sole purpose of redefining the Assets and
-// WebJarAssets classes in the Play's "controllers" route package. This way, all
-// controllers is normalized to exist inside the same package. This way, all
-// those crazy inconsistencies of usign "controllers" and
-// "es.uvigo.ei.sing.sds.controller" in both the routes
-// configuration file and in the views a removed.
-
-object AssetsController       extends AssetsBuilder(LazyHttpErrorHandler)
-object WebJarAssetsController extends WebJarAssets(Assets)
+object AssetsController extends controllers.AssetsBuilder(play.api.http.LazyHttpErrorHandler)
 
